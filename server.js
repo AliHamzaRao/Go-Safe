d9458f90ce6f0016aba43d09a94d04c2557c67dc
Ali <hamzaraoa010@gmail.com>
2021-11-14 22:30:32 +0500

    changes in server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/gradus'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/index.html'));
+        '/dist/gradus/index.html'));
 });
 app.listen(process.env.PORT || 8080);
