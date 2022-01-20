c1bef3c3cadd6d596289e275c42ca0582ddd8346
AliHamzaRao1 <ali.hamza@2bvision.com>
2022-01-20 20:14:11 +0500

    Paths Correction

--- src/app/routes/dashboard/dashboard.component.ts
+++ src/app/routes/dashboard/dashboard.component.ts
@@ -50,7 +50,7 @@ export class DashboardComponent implements OnInit {
   rectangle: any;
   circle: any;
   iconUrl: string =
-    "../../../assets/img/vendor/leaflet/new-icon/marker-icon.png";
+    "./assets/img/vendor/leaflet/new-icon/marker-icon.png";
   FenceParam: any;
   fenceName: any;
   countryName: any;
